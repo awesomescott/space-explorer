263c711d7e2cf6e60d534af0959a76e3d2c51d0c
Scott Gilmore <scott@plsol.com>
2021-03-09 18:46:45 -0500

    Hook up data sources to apollo server

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,7 +1,29 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 
-const server = new ApolloServer({ typeDefs });
+const { createStore } = require('./utils');
+
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+
+const store = createStore();
+
+const server = new ApolloServer({
+  typeDefs,
+
+  /**
+   * NOTE: If you use this.context in a datasource, it's critical to create a new instance in the dataSources function,
+   * rather than sharing a single instance.
+   * Otherwise, initialize might be called during the execution of asynchronous code for a particular user,
+   * replacing this.context with the context of another user.
+   * @returns new LaunchAPI and new UserAPI.
+   */
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store })
+  })
+});
 
 server.listen().then(() => {
   console.log(`
@@ -10,4 +32,3 @@ server.listen().then(() => {
     Explore at https://studio.apollographql.com/dev
   `);
 });
-
