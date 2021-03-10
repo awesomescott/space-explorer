67d55526b8fab3345087f36c39b0be936a9530da
Scott Gilmore <scott@plsol.com>
2021-03-10 17:03:21 -0500

    Define client-side schema

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -1,26 +1,63 @@
-import {
+import React from 'react';
+import ReactDOM from 'react-dom';import {
   ApolloClient,
   NormalizedCacheObject,
-  ApolloProvider
+  ApolloProvider,
+  gql,
+  useQuery,
 } from '@apollo/client';
+
 import { cache } from './cache';
-import React from 'react';
-import ReactDOM from 'react-dom';
+
+import Login from './pages/login';
 import Pages from './pages';
+
 import injectStyles from './styles';
+
+export const typeDefs = gql`
+  extend type Query {
+    isLoggedIn: Boolean!
+    cartItems: [ID!]!
+  }
+`;
 
 // Initialize ApolloClient
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
+  headers: {
+    authorization: localStorage.getItem('token') || '',
+  },
+  typeDefs,
   uri: 'http://localhost:4000/graphql',
 });
+
+/**
+ * Render our app
+ * - We wrap the whole app with ApolloProvider, so any component in the app can
+ *    make GraphqL requests. Our provider needs the client we created above,
+ *    so we pass it as a prop
+ * - We need a router, so we can navigate the app. We're using Reach router for this.
+ *    The router chooses between which component to render, depending on the url path.
+ *    ex: localhost:3000/login will render only the `Login` component
+ */
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
 
 injectStyles();
 
 // Pass the ApolloClient instance to the ApolloProvider component
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Pages />
+    <IsLoggedIn />
   </ApolloProvider>,
   document.getElementById('root')
 );
+
